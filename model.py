9bd48738fc2d5bc61642b354ab3b9c3795cf2ea3
Tetsuya Ishikawa <tiskw111@gmail.com>
2023-03-23 14:53:55 +0900

    Update: modify forwardX function of the GPB layer for faster training and add code for ResNet18

--- model.py
+++ model.py
@@ -7,20 +7,21 @@ PyTorch implementation of Gaussian process bootstrapping (GPB) layer and LeNet5 
 import collections
 
 import torch
+import torchvision
 
 
 class GaussianProcessBootstrapping(torch.nn.Module):
     """
     PyTorch implementation of Gaussian process bootstrapping layer.
     """
-    def __init__(self, std_error=1.0, alpha=0.9, scale=1.0, skip=100):
+    def __init__(self, std_error=1.0, alpha=0.9, scale=1.0, skip=200):
         """
         Constructor.
 
         Args:
-            std_error (float): Standerd deciation of measurement error.
-            alpha (float): Coefficient of moving average for the matrix P.
-            skip (int): Skip first `skip` steps because of the unstability of P.
+            std_error (float): Standerd deviation of measurement error.
+            alpha     (float): Coefficient of moving average for the matrix P.
+            skip      (int)  : Skip first `skip` steps because of the unstability of P.
         """
         torch.nn.Module.__init__(self)
         self.P = None
@@ -65,11 +66,18 @@ class GaussianProcessBootstrapping(torch.nn.Module):
             e = self.e
             P = self.P.clone().detach().double()
             I = torch.eye(P.shape[0], device=P.device, dtype=P.dtype)
-            S = I - torch.linalg.lstsq(P + e * I, P)[0]
+            S = I - torch.linalg.solve(P + e * I, P)
             M = (I - torch.matmul(P, S) / e).float()
 
-        V = torch.sum(torch.matmul(X, M) * X, dim=1, keepdim=True)
-        N = torch.sqrt(torch.clip(V, min=1.0E-10, max=None)) * torch.randn(X.shape, device=X.device)
+        # NOTE: In the above code, (P + e * I)^-1 @ P was solved by `torch.linalg.solve` function,
+        #       but we also be able to use `torch.linalg.lstsq` function instead. The author don't
+        #       know which function is better for this case.
+        #         >> S = I - torch.linalg.solve(P + e * I, P)    # Current code
+        #         >> S = I - torch.linalg.lstsq(P + e * I, P)[0] # This is also acceptable
+
+        with torch.no_grad():
+            V = torch.sum(torch.matmul(X, M) * X, dim=1, keepdim=True)
+            N = torch.sqrt(torch.clip(V, min=1.0E-10, max=None)) * torch.randn(X.shape, device=X.device)
 
         return X + self.k * N
 
@@ -80,7 +88,14 @@ class GaussianProcessBootstrapping(torch.nn.Module):
         Args:
             inputs (torch.tensor): Input tensor of shape (n_samples, n_channels1, n_channels2, ...).
         """
-        return self.forward_rank2(torch.flatten(X, start_dim=1)).reshape(X.shape)
+        # Convert tensor shape from (N, C, H, W) to (N, W, H, C).
+        X_3dim = torch.transpose(X, 1, 3)
+
+        # Convert tensor shape from (N, W, H, C) to (N*W*H, C).
+        X_2dim = X_3dim.reshape((-1, X.shape[1]))
+
+        # Apply GPB layer and restore the shape.
+        return torch.transpose(self.forward_rank2(torch.flatten(X_2dim, start_dim=1)).reshape(X_3dim.shape), 1, 3)
 
 
 def LeNet5(gpb_layer_pos="", std_error=1.0, skip=100):
@@ -133,6 +148,68 @@ def LeNet5(gpb_layer_pos="", std_error=1.0, skip=100):
     layers += [("block4_full", torch.nn.Linear(500, 10))]
 
     return torch.nn.Sequential(collections.OrderedDict(layers))
+
+
+def ResNet18(gpb_layer_pos="", std_error=0.1, scale=1.0, skip=100):
+    """
+    """
+    base_model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.IMAGENET1K_V1)
+
+    base_model.conv1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+    base_model.fc = torch.nn.Linear(512, 10)
+
+    layers = [
+        ("conv1",   base_model.conv1),
+        ("bn1",     base_model.bn1),
+        ("relu",    base_model.relu),
+        # ("maxpool", base_model.maxpool),
+    ]
+
+    if "0" in gpb_layer_pos:
+        layers += [("gpbl0", GaussianProcessBootstrapping(std_error=std_error, scale=scale, skip=skip))]
+
+    layers += [("layer1", base_model.layer1)]
+
+    if "1" in gpb_layer_pos:
+        layers += [("gpbl1", GaussianProcessBootstrapping(std_error=std_error, scale=scale, skip=skip))]
+
+    layers += [("layer2", base_model.layer2)]
+
+    if "2" in gpb_layer_pos:
+        layers += [("gpbl2", GaussianProcessBootstrapping(std_error=std_error, scale=scale, skip=skip))]
+
+    layers += [("layer3", base_model.layer3)]
+
+    if "3" in gpb_layer_pos:
+        layers += [("gpbl3", GaussianProcessBootstrapping(std_error=std_error, scale=scale, skip=skip))]
+
+    layers += [("layer4", base_model.layer4)]
+
+    if "4" in gpb_layer_pos:
+        layers += [("gpbl4", GaussianProcessBootstrapping(std_error=std_error, scale=scale, skip=skip))]
+
+    layers += [
+        ("avgpool", base_model.avgpool),
+        ("flatten", torch.nn.Flatten()),
+    ]
+
+    if "5" in gpb_layer_pos:
+        layers += [("gpbl5", GaussianProcessBootstrapping(std_error=std_error, scale=scale, skip=skip))]
+
+    layers += [
+        ("fc", base_model.fc),
+    ]
+
+    return torch.nn.Sequential(collections.OrderedDict(layers))
+
+
+def update_gpbl_scale(model, scale):
+
+    for layer in model.modules():
+        if layer is isinstance(layer, GaussianProcessBootstrapping):
+            layer.k = scale
+
+    return model
 
 
 # vim: expandtab tabstop=4 shiftwidth=4 fdm=marker
--- train.py
+++ train.py
@@ -7,175 +7,222 @@ Training script of the Gaussian process bootstrapping layer (GPB layer).
 import argparse
 import time
 
-import sklearn.metrics
+import rich.progress
 import torch
+import torchmetrics
 import torchvision
 
-from model import LeNet5
+from model import LeNet5, ResNet18
 
 
 def parse_args():
     """
     Parse command line arguments.
     """
-    parser = argparse.ArgumentParser(description=__doc__.strip())
-    parser.add_argument("--batch_size", default=500, type=int, help="batch size")
-    parser.add_argument("--data_aug", action="store_true", help="enable data augmentation")
-    parser.add_argument("--device", default="cuda", type=str, help="device type for NN model")
-    parser.add_argument("--epoch", default=300, type=int, help="number of training epochs")
-    parser.add_argument("--gpb_layer_pos", default="", type=str, help="number of GPB layers")
-    parser.add_argument("--log_interval", default=10, type=int, help="logging interval")
-    parser.add_argument("--n_cpus", default=4, type=int, help="number of available CPUs")
-    parser.add_argument("--save", default="", type=str, help="save trained model")
-    parser.add_argument("--std_error", default=0.2, type=float, help="standard deviation of observation")
+    # Create parser instance.
+    fmtcls = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog, max_help_position=32)
+    parser = argparse.ArgumentParser(description=__doc__.strip(), formatter_class=fmtcls, add_help=False)
+
+    # Define group 1: model options.
+    group1 = parser.add_argument_group("Model options")
+    group1.add_argument("--model", metavar="NAME", type=str, default="resnet18",
+                        help="name of neural network model")
+
+    # Define group 2: GPB layer options.
+    group2 = parser.add_argument_group("GPB layer options")
+    group2.add_argument("--gpb_dec_epochs", metavar="INT", default=None, type=int,
+                        help="decrease GPBLayer effect last few epochs")
+    group2.add_argument("--gpb_layer_pos", metavar="STR", default="", type=str,
+                        help="position of GPB layers in a neural network")
+    group2.add_argument("--gpb_max_scale", metavar="FLT", type=float, default=1.0,
+                        help="scale of noise applied in GPB layers")
+    group2.add_argument("--gpb_std_error", metavar="FLT", type=float, default=0.2,
+                        help="standard deviation of observation")
+
+    # Define group 3: dataset options.
+    group3 = parser.add_argument_group("Dataset options")
+    group3.add_argument("--dataset", metavar="NAME", type=str, default="cifar10",
+                        help="dataset name")
+
+    # Define group 4: training options.
+    group4 = parser.add_argument_group("Training options")
+    group4.add_argument("--batch_size", metavar="INT", type=int, default=250,
+                        help="size of batch")
+    group4.add_argument("--data_aug", action="store_true",
+                        help="enable data augmentation")
+    group4.add_argument("--device", metavar="STR", type=str, default="cuda",
+                        help="device type for NN model")
+    group4.add_argument("--epochs", metavar="INT", type=int, default=200,
+                        help="number of training epochs")
+    group4.add_argument("--n_cpus", metavar="INT", type=int, default=4,
+                        help="number of available CPUs")
+    group4.add_argument("--save", metavar="PATH", type=str, default=None,
+                        help="path to save trained model")
+
+    # Define group 5: other options.
+    group5 = parser.add_argument_group("Other options")
+    group5.add_argument("-h", "--help", action="help",
+                        help="show this message and exit")
+    group5.add_argument("-v", "--version", action="version", version="",
+                        help="show version info and exit")
+
     return parser.parse_args()
 
 
-def train(model, args, quiet=False, alpha_avg_loss=0.9):
+def train(model, dataloader, loss_func, optimizer, scheduler=None):
     """
     Train NN model.
 
     Args:
-        model (torch.nn.Module): NN model to be trained.
-        args (argpaese.Namespace): Command line arguments.
-        quiet (bool): If true, no training log is dumped.
-        alpha_avg_loss (float): Coefficient of moving average for loss values.
+        model      (torch.nn.Module)            : Target model.
+        dataloader (torch.utils.data.DataLoader): Data loader.
+        loss_func  (torch.nn.Module)            : Loss function.
+        optimizer  (torch.optim.Optimizer)      : Optimizer.
+        scheduler  (torch.optim.LRScheduler)    : Learning rate scheduler.
     """
-    model.to(args.device)
+    # Get target device name from the model.
+    device = next(model.parameters()).device
 
-    # Setup training dataset.
-    transforms = [
-        torchvision.transforms.RandomCrop(32, padding=4),
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ]
-    transform = torchvision.transforms.Compose(transforms if args.data_aug else transforms[2:])
-    dataset = torchvision.datasets.CIFAR10("dataset", train=True, transform=transform, download=True)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.n_cpus)
+    # Define and instanciate metrics.
+    metrics = {"train/loss": torchmetrics.MeanMetric(),
+               "train/acc" : torchmetrics.Accuracy(task="multiclass", num_classes=10)}
 
-    # Setup optimizer.
-    optimizer = torch.optim.Adam(model.parameters(), lr=1.0E-3)
+    # Transition to traning mode.
+    model.train()
 
-    # Initialize average loss.
-    avg_loss = 0.0
+    # Start traninig loop.
+    for images, labels in rich.progress.track(dataloader, total=len(dataloader), transient=True):
 
-    time_start = time.time()
+        # Move images/labels to the target device.
+        images = images.to(device)
+        labels = labels.to(device)
 
-    for epoch in range(args.epoch):
+        # Initialize optimizer.
+        optimizer.zero_grad()
 
-        model.train()
+        # Compute model output and loss value.
+        output = model(images)
+        loss   = loss_func(output, labels)
 
-        for step, (images, labels) in enumerate(dataloader):
+        # Run back propergation.
+        loss.backward()
+        optimizer.step()
 
-            images = images.to(args.device)
-            labels = labels.to(args.device)
+        # Step learning rate scheduler.
+        if scheduler is not None:
+            scheduler.step()
 
-            optimizer.zero_grad()
+        # Update metrics.
+        metrics["train/loss"].update(loss.to("cpu"))
+        metrics["train/acc"].update(output.to("cpu"), labels.to("cpu"))
 
-            # Compute model output and loss value.
-            output = model(images)
-            loss = torch.nn.functional.cross_entropy(output, labels, reduction="mean")
-
-            # Update average loss.
-            avg_loss = alpha_avg_loss * avg_loss + (1.0 - alpha_avg_loss) * float(loss)
-
-            # Run back propergation.
-            loss.backward()
-            optimizer.step()
-
-            time_elapsed = time.time() - time_start
-
-            # Dump log.
-            if (not quiet) and (step % args.log_interval == 0):
-                messages = [
-                    f"epoch = {epoch:d}",
-                    f"step = {step:d}",
-                    f"loss = {loss:.4f}",
-                    f"avg_loss = {avg_loss:.4f}",
-                    f"time_elapsed = {time_elapsed:.1f} [sec]",
-                ]
-                print("Train: " + "\t".join(messages))
-
-        # Run test procedure.
-        test(model, args)
+    # Returns metrics.
+    return {key:float(metric.compute()) for key, metric in metrics.items()}
 
 
-def test(model, args):
+def test(model, dataloader, loss_func):
     """
     Test NN model.
 
     Args:
-        model (torch.nn.Module): NN model to be tested.
-        args (argpaese.Namespace): Command line arguments.
+        model      (torch.nn.Module)            : Target model.
+        dataloader (torch.utils.data.DataLoader): Data loader.
+        loss_func  (torch.nn.Module)            : Loss function.
     """
-    model.to(args.device)
+    # Get target device name from the model.
+    device = next(model.parameters()).device
+
+    # Define and instanciate metrics.
+    metrics = {"test/loss": torchmetrics.MeanMetric(),
+               "test/acc" : torchmetrics.Accuracy(task="multiclass", num_classes=10)}
+
+    # Transition to evaluation mode.
     model.eval()
 
-    # Setup test dataset.
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    dataset = torchvision.datasets.CIFAR10(root="dataset", train=False, transform=transform)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=args.n_cpus)
+    # Start test loop.
+    for images, labels in rich.progress.track(dataloader, total=len(dataloader), transient=True):
 
-    # Initialize values that are necessary for computing test scores.
-    num_dat = 0
-    lossval = 0.0
-    ys_pred = list()
-    ys_true = list()
+        # Move images/labels to the target device.
+        images = images.to(device)
+        labels = labels.to(device)
 
-    for images, labels in dataloader:
-
-        images = images.to(args.device)
-        labels = labels.to(args.device)
-
-        # Compute model output.
+        # Compute model output and loss value.
         output = model(images)
+        loss   = loss_func(output, labels)
 
-        # Update values.
-        num_dat += images.shape[0]
-        lossval += torch.nn.functional.cross_entropy(output, labels, reduction="sum")
-        ys_pred += torch.argmax(output, dim=1).tolist()
-        ys_true += labels.tolist()
+        # Update metrics.
+        metrics["test/loss"].update(loss.to("cpu"))
+        metrics["test/acc"].update(output.to("cpu"), labels.to("cpu"))
 
-    # Compute test scores.
-    scores = {
-        "accuracy" : sklearn.metrics.accuracy_score(ys_true, ys_pred),
-        "precision": sklearn.metrics.precision_score(ys_true, ys_pred, average="macro"),
-        "recall"   : sklearn.metrics.recall_score(ys_true, ys_pred, average="macro"),
-        "f1"       : sklearn.metrics.f1_score(ys_true, ys_pred, average="macro"),
-        "loss"     : lossval / num_dat,
-    }
-
-    # Dump log.
-    messages = [
-        f"accuracy = {scores['accuracy']:.4f}",
-        f"precision = {scores['precision']:.4f}",
-        f"recall = {scores['recall']:.4f}",
-        f"f1 = {scores['f1']:.4f}",
-        f"loss = {scores['loss']:.4f}",
-    ]
-    print("Test: " + "\t".join(messages))
+    # Returns metrics.
+    return {key:float(metric.compute()) for key, metric in metrics.items()}
 
 
 def main(args):
     """
-    Main function.
+    Entry point of this script.
+
+    Args:
+        args (argparse.Namespace): Parsed command line arguments.
     """
     # Dump arguments.
     print("args =", args)
 
     # Create NN model instance.
-    model = LeNet5(gpb_layer_pos=args.gpb_layer_pos, std_error=args.std_error)
+    # model = LeNet5(gpb_layer_pos=args.gpb_layer_pos, std_error=args.std_error)
+    model = ResNet18(gpb_layer_pos=args.gpb_layer_pos, std_error=args.gpb_std_error)
+    model.to(args.device)
     print(model)
 
-    # Start training procedure.
-    train(model, args)
+    # Setup transforms.
+    transform_train = torchvision.transforms.Compose([
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomRotation(15),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    transform_test = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    # Setup dataset.
+    ds_common_args = {"download": True}
+    dataset_train  = torchvision.datasets.CIFAR10("dataset", train=True,  transform=transform_train, **ds_common_args)
+    dataset_test   = torchvision.datasets.CIFAR10("dataset", train=False, transform=transform_test,  **ds_common_args)
+
+    # Setup data loader.
+    dl_common_args   = {"batch_size": args.batch_size, "num_workers": args.n_cpus}
+    dataloader_train = torch.utils.data.DataLoader(dataset_train, shuffle=True,  **dl_common_args)
+    dataloader_test  = torch.utils.data.DataLoader(dataset_test , shuffle=False, **dl_common_args)
+
+    # Define loss function.
+    loss_func = torch.nn.CrossEntropyLoss(reduction="mean", label_smoothing=0.1)
+
+    # Setup optimizer.
+    optimizer = torch.optim.SGD(model.parameters(), lr=1.0E-4, momentum=0.9, weight_decay=5.0E-4)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, steps_per_epoch=len(dataloader_train), epochs=args.epochs)
+
+    for epoch in range(1, args.epochs + 1):
+
+        # Change scale of GPB layers if decreasing epochs specified.
+        if args.gpb_dec_epochs is not None and (epoch > (args.epochs - args.gpb_dec_epochs)):
+            scale = (args.epochs - epoch) / args.gpb_dec_epochs
+            model = update_gpbl_scale(model, scale)
+
+        # Train the model for one epoch and get metrics.
+        result_train = train(model, dataloader_train, loss_func, optimizer, scheduler)
+
+        # Test the model and get metrics.
+        result_test = test(model, dataloader_test, loss_func)
+
+        # Print metrics.
+        result = dict(**result_train, **result_test)
+        print("\t".join(["[%03d]" % epoch] + [key + "=" + str(val)[:10].rjust(10) for key, val in result.items()]))
 
     # Save model.
-    if args.save:
+    if args.save is not None:
         torch.save(model.to("cpu").state_dict(), args.save)
 
 
